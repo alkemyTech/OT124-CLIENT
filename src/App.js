907a944f7a69b8ca127fad976a6f0926d44ed867
Nicolas-Marsan <nicomar80@gmail.com>
2022-01-22 11:21:06 -0300

    Install RactRouterDom and create / and /nosotros

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import Home from "./componets/Home";
+import OboutUs from "./componets/OboutUs";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/nosotros" element={<OboutUs/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/componets/OboutUs.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function OboutUs(props) {
+  return (
+    <div className="Home">
+      <div>
+        <p>Hello!, we are an ....</p>
+      </div>
+    </div>
+  );
+}
+
+export default OboutUs;
