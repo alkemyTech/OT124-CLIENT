b7d0e68c616a4b4e11de5e11194312d45f5b24a8
Nicolas-Marsan <62968906+Nicolas-Marsan@users.noreply.github.com>
2022-01-24 19:24:23 -0300

    Install RactRouterDom and create / and /nosotros (#3)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Home from "./componets/Home";
+import OboutUs from "./componets/OboutUs";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/nosotros" element={<OboutUs/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
