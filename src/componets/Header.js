e11808aff6ecd8a416f1c306e7199594859f2c36
necogamy <madrazonecogamy@gmail.com>
2022-01-25 17:39:44 -0300

    Header styles added with tailwin

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -3,23 +3,27 @@ import { Link } from 'react-router-dom';
 
 function Header() {
     const navItems = [
-        <Link to='/inicio'>Inicio</Link>, 
-        <Link to='/nosotros'>Nosotros</Link>,
-        <Link to='/actividades'>Actividades</Link>,
-        <Link to='/novedades'>Novedades</Link>,
-        <Link to='/testimonios'>Testimonios</Link>,
-        <Link to='/contacto'>Contacto</Link>,
-        <Link to='/contribuye'>Contribuye</Link>
+        <Link className='mr-6 text-sky-500' to='/inicio'>Inicio</Link>, 
+        <Link className='mr-6 text-sky-500' to='/nosotros'>Nosotros</Link>,
+        <Link className='mr-6 text-sky-500' to='/actividades'>Actividades</Link>,
+        <Link className='mr-6 text-sky-500' to='/novedades'>Novedades</Link>,
+        <Link className='mr-6 text-sky-500' to='/testimonios'>Testimonios</Link>,
+        <Link className='mr-6 text-sky-500' to='/contacto'>Contacto</Link>,
+        <Link className='mr-6 text-sky-500' to='/contribuye'>Contribuye</Link>
     ];
 
     return (
-        <header>
-            <img src='' alt='logo' />
+        <header className='flex items-center py-1.5'>
+            <img className='ml-4 mr-8' width='100' src='https://cdn.worldvectorlogo.com/logos/free-logo.svg' alt='logo' />
             <nav>
                 {
                     navItems
                 }
             </nav>
+            <div className='flex ml-32'>
+                <button className='bg-transparent hover:bg-sky-500 text-sky-500 font-semibold hover:text-white py-1.5 px-6 border border-sky-500 hover:border-transparent rounded mr-3'>Log in</button>
+                <button className='bg-sky-500 hover:bg-sky-700 text-white font-bold py-1.5 px-6 rounded'>Registrate</button>
+            </div>
         </header>
     );
 }
