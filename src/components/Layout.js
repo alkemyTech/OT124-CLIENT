20ff6c150cd7c8c087ec780d2d43eec239a71a87
Nicolás López <36177527+necogamy@users.noreply.github.com>
2022-01-27 15:59:14 -0300

    Ot124 21 (#7)
    * Added react-router-dom required library to the project
    
    * Setup, component created and added to the App.js
    
    * Header styles added with tailwin
    
    * Header component requeriments made

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "./Footer";
 import { Outlet } from "react-router";
+import Header from './Header';
 
 export default function Layout() {
   return (
     <div>
+      <Header />
       <div>
         <Outlet></Outlet>
       </div>
