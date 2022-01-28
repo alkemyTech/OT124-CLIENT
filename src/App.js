d999a7a2b7a085346688d539b6d9e72ef0922e76
Rafael Lynch <rafalynch.dev@gmail.com>
2022-01-28 17:03:33 -0300

    Create login page & login form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import Layout from "./components/Layout";
+import Login from "./pages/Login";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/nosotros" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
   );
