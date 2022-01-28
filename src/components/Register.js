eee233558e2faa64ed6bc80ff7afa9cbb8a58327
necogamy <madrazonecogamy@gmail.com>
2022-01-28 17:46:05 -0300

    Changed manual validation with yup

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
+
+const registerSchema = yup.object().shape({
+  name: yup
+    .string()
+    .required('Required'),
+  surname: yup
+    .string()
+    .required('Required'),
+  email: yup
+    .string()
+    .required('Required')
+    .email("Invalid email")
+    .max(255),
+  password: yup
+    .string()
+    .required('Required')
+    .min(6, "Too Short!")
+    .max(50, "Too Long!")
+});
+
 
 export default function Register() {
     const [ formData, setFormData ] = useState({});
@@ -11,24 +33,7 @@ export default function Register() {
     return (
         <Formik
             initialValues={{ name: '', surname: '', email: '', password: '' }}
-            validate={values => {
-                const errors = {};
-
-                if (!values.email) {
-                    errors.email = 'Required';
-                } else if (!values.name) {
-                    errors.name = 'Required';
-                } else if (!values.surname) {
-                    errors.surname = 'Required';
-                } else if (!values.password) {
-                    errors.password = 'Required';
-                }
-
-                if (values.password.length < 6) errors.password = 'Password must have more than 6 characters';
-                if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) errors.email = 'Invalid email address';
-                
-                return errors;
-        }}
+            validationSchema={registerSchema}
             onSubmit={(values, { setSubmitting }) => {
                 setTimeout(() => {
                   setFormData(values);
