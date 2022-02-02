67d23365f72baec5fdaa2ea47425855b53e0b0af
Franco Martin <francomartin.personal@gmail.com>
2022-02-02 17:57:16 -0300

    navlink header format

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Link, useNavigate,NavLink} from 'react-router-dom';
+import { Link, useNavigate, NavLink } from 'react-router-dom';
 import { deleteUserData } from '../features/authSlice';
 
 export default function Header() {
@@ -9,13 +9,13 @@ export default function Header() {
     let activeClassName = "mr-6 text-slate-300";
     let desactiveClassName = "mr-6 text-sky-500";
     const navItems = [
-        <NavLink  exact to='/'className={({ isActive }) =>isActive ? activeClassName : desactiveClassName }>Inicio</NavLink >, 
-        <NavLink className={({ isActive }) =>isActive ? activeClassName : desactiveClassName} to='/nosotros'>Nosotros</NavLink >,
-        <NavLink className={({ isActive }) =>isActive ? activeClassName : desactiveClassName} to='/actividades'>Actividades</NavLink >,
-        <NavLink className={({ isActive }) =>isActive ? activeClassName : desactiveClassName } to='/novedades'>Novedades</NavLink >,
-        <NavLink className={({ isActive }) =>isActive ? activeClassName : desactiveClassName } to='/testimonios'>Testimonios</NavLink >,
-        <NavLink  className={({ isActive }) =>isActive ? activeClassName : desactiveClassName} exact to='/contacto'>Contacto</NavLink >,
-        <NavLink className={({ isActive }) =>isActive ? activeClassName : desactiveClassName} to='/contribuye'>Contribuye</NavLink >
+        <NavLink exact to='/' className={({ isActive }) => isActive ? activeClassName : desactiveClassName}>Inicio</NavLink >,
+        <NavLink className={({ isActive }) => isActive ? activeClassName : desactiveClassName} to='/nosotros'>Nosotros</NavLink >,
+        <NavLink className={({ isActive }) => isActive ? activeClassName : desactiveClassName} to='/actividades'>Actividades</NavLink >,
+        <NavLink className={({ isActive }) => isActive ? activeClassName : desactiveClassName} to='/novedades'>Novedades</NavLink >,
+        <NavLink className={({ isActive }) => isActive ? activeClassName : desactiveClassName} to='/testimonios'>Testimonios</NavLink >,
+        <NavLink className={({ isActive }) => isActive ? activeClassName : desactiveClassName} exact to='/contacto'>Contacto</NavLink >,
+        <NavLink className={({ isActive }) => isActive ? activeClassName : desactiveClassName} to='/contribuye'>Contribuye</NavLink >
     ];
 
     const userData = 's'
@@ -33,14 +33,14 @@ export default function Header() {
             </nav>
             <div className='flex ml-32'>
                 {!userData ?
-                <>
-                <button className='bg-transparent hover:bg-sky-500 text-sky-500 font-semibold hover:text-white py-1.5 px-6 border border-sky-500 hover:border-transparent rounded mr-3'>Log in</button>
-                <button className='bg-sky-500 hover:bg-sky-700 text-white font-bold py-1.5 px-6 rounded'>Registrate</button>
-                </>
-                : 
-                <button onClick={handleCloseSesion} className='bg-transparent hover:bg-sky-500 text-sky-500 font-semibold hover:text-white py-1.5 px-6 border border-sky-500 hover:border-transparent rounded mr-3'>Cerrar Sesion</button>
+                    <>
+                        <button className='bg-transparent hover:bg-sky-500 text-sky-500 font-semibold hover:text-white py-1.5 px-6 border border-sky-500 hover:border-transparent rounded mr-3'>Log in</button>
+                        <button className='bg-sky-500 hover:bg-sky-700 text-white font-bold py-1.5 px-6 rounded'>Registrate</button>
+                    </>
+                    :
+                    <button onClick={handleCloseSesion} className='bg-transparent hover:bg-sky-500 text-sky-500 font-semibold hover:text-white py-1.5 px-6 border border-sky-500 hover:border-transparent rounded mr-3'>Cerrar Sesion</button>
                 }
-                
+
             </div>
         </header>
     );
