554c2472fc74da8d6753e66a3e4a3d74f9b6ff19
Rafael Lynch <rafalynch.dev@gmail.com>
2022-02-03 20:06:01 -0300

    Add login reducer

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,24 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState: {
-      userData: null,
+  name: "auth",
+  initialState: {
+    userData: null,
+    isLoggedIn: false,
+  },
+  reducers: {
+    setUserData: (state, action) => {
+      state.userData = action.payload;
+      localStorage.setItem("token", JSON.stringify(state.userData));
     },
-    reducers: {
-      setUserData: (state, action) => {
-        state.userData = action.payload
-        localStorage.setItem('token', JSON.stringify(state.userData))
-      },
-      deleteUserData: (state) => {
-        localStorage.removeItem('token')
-        state.userData = null
-      },
+    deleteUserData: (state) => {
+      localStorage.removeItem("token");
+      state.userData = null;
     },
-})
+    logIn: (state) => {
+      localStorage.setItem("isLoggedIn", true);
+      state.isLoggedIn = true;
+    },
+  },
+});
 
-export const { setUserData, deleteUserData } = authSlice.actions
+export const { setUserData, deleteUserData, logIn } = authSlice.actions;
 
-export const selectUserData = (state) => state.auth.userData
+export const selectUserData = (state) => state.auth.userData;
 
-export default authSlice.reducer
+export default authSlice.reducer;
