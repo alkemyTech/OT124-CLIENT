bfb647f6ca54eb10402eeddc5cfcaaee1f8193f2
Nicolas-Marsan <nicomar80@gmail.com>
2022-02-03 19:35:26 -0300

    Fix conflict home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,62 +1,66 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Carrusel from "./Carrusel";
 
 function Box() {
   const styles = {
     width: 200,
     height: 150,
-    backgroundColor: 'gray'
-  }
+    backgroundColor: "gray",
+  };
 
   return <div style={styles}></div>;
 }
 
 function Home(props) {
-  const welcome = 'Texto de bienvenida';
+  const welcome = "Texto de bienvenida";
 
   return (
     <div className="Home flex flex-col justify-center items-center text-center">
+      <Carrusel />
 
-        <Carrusel />
+      <h1 className="text-2xl">{welcome}</h1>
 
-        <h1 className='text-2xl'>{welcome}</h1>
-
-        <section className='mt-16'>
-          <h2 className='pb-10 text-xl'>Últimas novedades</h2>
-          <div className='flex gap-9'>
-            <div className='flex gap-9 flex-col md:flex-row'>
-              <Box />
-              <Box />
-            </div>
-            <div className='flex gap-9 flex-col md:flex-row'>
-              <Box />
-              <Box />
-            </div>
+      <section className="mt-16">
+        <h2 className="pb-10 text-xl">Últimas novedades</h2>
+        <div className="flex gap-9">
+          <div className="flex gap-9 flex-col md:flex-row">
+            <Box />
+            <Box />
           </div>
-          <div className='flex mt-12 items-center'>
-            <div className='pl-6'>
-              <Link className='text-sky-500 pr-5' to='/testimonios'>Testimonios</Link>
-              <Link className='text-sky-500' to='/contacto'>Contacto</Link>
-            </div>
-            <button className='bg-transparent hover:bg-sky-500 text-sky-500 font-semibold hover:text-white py-1.5 px-6 border border-sky-500 hover:border-transparent rounded m-auto'>Ver todas</button>
+          <div className="flex gap-9 flex-col md:flex-row">
+            <Box />
+            <Box />
           </div>
-        </section>
-
-        <section className='mt-24'>
-          <h2 className='pb-10 text-xl'>Testimonios</h2>
-          <div className='flex gap-9'>
-            <div className='flex gap-9 flex-col md:flex-row'>
-              <Box />
-              <Box />
-            </div>
-            <div className='flex gap-9 flex-col md:flex-row'>
-              <Box />
-              <Box />
-            </div>
+        </div>
+        <div className="flex mt-12 items-center">
+          <div className="pl-6">
+            <Link className="text-sky-500 pr-5" to="/testimonios">
+              Testimonios
+            </Link>
+            <Link className="text-sky-500" to="/contacto">
+              Contacto
+            </Link>
           </div>
-        </section>
+          <button className="bg-transparent hover:bg-sky-500 text-sky-500 font-semibold hover:text-white py-1.5 px-6 border border-sky-500 hover:border-transparent rounded m-auto">
+            Ver todas
+          </button>
+        </div>
+      </section>
 
+      <section className="mt-24">
+        <h2 className="pb-10 text-xl">Testimonios</h2>
+        <div className="flex gap-9">
+          <div className="flex gap-9 flex-col md:flex-row">
+            <Box />
+            <Box />
+          </div>
+          <div className="flex gap-9 flex-col md:flex-row">
+            <Box />
+            <Box />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
