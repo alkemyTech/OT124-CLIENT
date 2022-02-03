d4d7ed137e87f5bf55c25a3ccee70785411d2413
Rafael Lynch <rafalynch.dev@gmail.com>
2022-02-03 17:59:23 -0300

    Add responsive design

--- src/components/BackofficeCard.js
+++ src/components/BackofficeCard.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 export default function BackofficeCard(props) {
   return (
-    <div className="w-48 h-48 border-1 rounded-lg p-6 grid justify-items-center gap-2 shadow-lg hover:shadow-2xl">
+    <div className="md:w-48 w-32 md:h-48 h-32 border-1 rounded-lg p-2 md:p-6 grid justify-items-center gap-2 shadow-lg hover:shadow-2xl">
       <span>{props.title}</span>
-      <img src={props.image} className="h-20"></img>
+      <img src={props.image} className="h-10 md:h-20"></img>
       <Link
-        className="bg-sky-500 hover:bg-sky-700 text-white font-bold px-4 rounded"
+        className="bg-sky-500 hover:bg-sky-700 text-white font-bold text-xs md:text-sm px-2 md:px-4 rounded"
         to={props.navlink}
       >
         IR
