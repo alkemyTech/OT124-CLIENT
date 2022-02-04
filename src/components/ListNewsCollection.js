fb600e268335d268928158764ac51faf1c4e7734
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-02-04 17:37:15 -0300

    Creating 404 and basic layout

--- src/components/ListNewsCollection.js
+++ src/components/ListNewsCollection.js
@@ -1,6 +1,4 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import NewsDetail from "../pages/NewsDetail";
 import React from "react";
 
 
@@ -8,7 +6,7 @@ export default function NewsCollection() {
 
     const collectNews = [
         {name: "Something One", content: "Bullshit I thought about" , id: 1},
-        {name: "Something Two", content: "Bullshit I thought about", id: 2}
+        {name: "Something Two", content: "lorem ipsum calius domo ir deberis dim callus domus elr", id: 2}
     ]
 
     const newsID = useParams();
@@ -23,16 +21,27 @@ export default function NewsCollection() {
      */
 
     const desiredItem = collectNews.filter((e) => e.id == newsID.id)
-    console.log(desiredItem)
     
     return (
-    <section className=""> 
-        {desiredItem == undefined ? 
-        "These i nothing to see" : 
-        <>
-            <h2>Hello, this is what we found: {desiredItem[0].name}</h2>
-            <p>We have more information here: {desiredItem[0].content}</p>
-        </>
+    <section className=" hover:shadow-lg rounded-sm p-2 md:p-10 md:m-28 shadow-md "> 
+        {desiredItem[0] == undefined ? 
+            <div className=" grid justify-items-start grid-rows-2 grid-cols-[minmax(75px,75px),minmax(1fr,1fr)]">
+                <div class=" flex justify-self-center justify-center items-center row-start-1 row-end-3 -space-x-1 overflow-hidden"> 
+                    <img class=" shadow-md inline-block h-16 w-16 md:h-24 md:w-24 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""></img>
+                </div>
+            <h2 className=" col-start-2 col-end-3 font-bold text-xl">Algo esta mal</h2>
+            <p className="col-start-2 col-end-3">La novedad que estas buscando no existe.</p>
+        
+        </div>
+         : 
+        <div className=" grid justify-items-start grid-rows-2 grid-cols-[minmax(75px,75px),minmax(1fr,1fr)]">
+            <div class=" flex justify-self-center justify-center items-center row-start-1 row-end-3 -space-x-1 overflow-hidden"> 
+                <img class=" shadow-md inline-block h-16 w-16 md:h-24 md:w-24 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""></img>
+            </div>
+            <h2 className=" self-end p-2 col-start-2 col-end-3 font-bold text-xl">{desiredItem[0].name}</h2>
+            <p className=" p-2 col-start-2 col-end-3">This is the content of the new: {desiredItem[0].content}</p>
+            
+        </div>
         }
     </section>
     )
