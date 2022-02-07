55c4ba3468180367170d03f9619481494990ab75
Diego Sosa <84159968+dhsosa98@users.noreply.github.com>
2022-02-06 23:41:16 -0300

    Created UploadImageComponents - SVGSpin - CUNewsForm - Fixed routes Issues - OT124-54 (#27)
    * added react-dropzone dependency
    
    * updated index.html
    
    * added CUNewsForm component
    
    * fixed routes issues in backoffice/index
    
    * formated contacForm component
    
    * created SpinSVGButton Component
    
    * Created and styled UploadImageComponent
    
    * created news service
    
    * created the createMultiForm helper
    
    * Created and styled CUNewsForm component
    
    * Fixed backoffice directory name

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import { Route, Routes } from "react-router-dom";
 import ContactUs from "./pages/ContactUs";
-import BackofficeCategories from "./pages/backOffice/categories/BackofficeCategories";
-import BackOffice from "./components/BackOffice";
-import BackOfficeHome from "./pages/backOffice";
+import BackofficeCategories from "./pages/backoffice/categories/BackofficeCategories";
+import Backoffice from "./components/Backoffice";
+import BackofficeHome from "./pages/backoffice";
+import CUNewsForm from "./components/CUNewsForm";
 
 function App() {
   return (
@@ -19,9 +20,10 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="contacto" element={<ContactUs />} />
         <Route path="signup" element={<SignUp />} />
-        <Route path="backoffice" element={<BackOffice />}>
-          <Route index element={<BackOfficeHome />} />
+        <Route path="backoffice" element={<Backoffice />}>
+          <Route index element={<BackofficeHome />} />
           <Route path="categories" element={<BackofficeCategories />} /> 
+          <Route path="news/:id" element={<CUNewsForm />} />
         </Route>
       </Route>
     </Routes>
