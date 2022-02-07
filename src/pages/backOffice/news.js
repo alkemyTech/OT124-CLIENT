b0726853c951166b0259b9d71bf2b5d20c9875a3
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-02-07 18:34:50 -0300

    Adjust Paddding

--- src/pages/backOffice/news.js
+++ src/pages/backOffice/news.js
@@ -18,7 +18,7 @@ export default function BackofficeNews() {
 
     return (
         <section className=" grid justify-items-center grid-cols-1 md:p-12">
-            <div className=" md:w-48 w-32 md:h-48 h-fit border-1 rounded-lg p-2 md:p-6 grid justify-items-center gap-2 shadow-sm hover:shadow-lg">
+            <div className=" md:w-48 w-32 md:h-48 h-fit border-1 rounded-lg p-2 md:p-6 grid justify-items-center gap-2 shadow-lg">
                 <img className=" p-1 w-28 " src={newsIcon}></img>
                 <button className="bg-sky-500 hover:bg-sky-700 text-white font-bold text-xs md:text-sm py-1 px-2 md:px-4 rounded"><Link to={"/backoffice"}>VOLVER</Link></button>
             </div>
@@ -27,7 +27,7 @@ export default function BackofficeNews() {
                     return(
                         <div className=" grid h-fit md:grid-cols-[0.5fr_1fr] grid-rows-[0.5fr_1fr_0.5fr] grid-cols-1 flex-col mx-6 my-6  md:h-60 border-1 rounded-lg p-2 md:p-6 shadow-lg hover:shadow-2xl">
                             <img className="hidden md:inline-block col-start-1 col-end-2 row-start-1 row-end-4 bg-slate-500"></img>
-                            <h3 className=" text-xl">{element}</h3>
+                            <h3 className=" p-1 text-xl">{element}</h3>
                             <p className="p-1">Aqui hay poco contenido descriptivo, pero que mala suerte, yo queria más</p>
                             <button className="bg-sky-500 w-fit self-center py-1 justify-self-center hover:bg-sky-700 text-white font-bold text-xs md:text-sm px-2 md:px-4 rounded"><Link to={"/backoffice"}>MÁS INFORMACION</Link></button>
                         </div>
