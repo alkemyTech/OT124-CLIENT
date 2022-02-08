d263baf8df40589db0d04e5f89e152028c60a433
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-02-07 21:45:19 -0300

    Add extra button

--- src/pages/backOffice/news.js
+++ src/pages/backOffice/news.js
@@ -22,14 +22,18 @@ export default function BackofficeNews() {
                 <img className=" p-1 w-28 " src={newsIcon}></img>
                 <button className="bg-sky-500 hover:bg-sky-700 text-white font-bold text-xs md:text-sm py-1 px-2 md:px-4 rounded"><Link to={"/backoffice"}>VOLVER</Link></button>
             </div>
-            <div className="flex justify-center flex-wrap md:p-2">
+            <div className="flex justify-center md:p-2">
                 {newsArray.map((element) => {
                     return(
-                        <div className=" grid h-fit md:grid-cols-[0.5fr_1fr] grid-rows-[0.5fr_1fr_0.5fr] grid-cols-1 flex-col mx-6 my-6  md:h-60 border-1 rounded-lg p-2 md:p-6 shadow-lg hover:shadow-2xl">
-                            <img className="hidden md:inline-block col-start-1 col-end-2 row-start-1 row-end-4 bg-slate-500"></img>
+                        <div className=" grid  h-fit md:grid-cols-[0.5fr_1fr] grid-rows-[0.5fr_1fr_0.5fr] grid-cols-1 flex-col mx-6 my-6  md:h-60 border-1 rounded-lg p-2 md:p-6 shadow-lg hover:shadow-2xl">
+                            <img className="md:inline-block col-start-1 col-end-2 row-start-1 row-end-4 bg-slate-500"></img>
                             <h3 className=" p-1 text-xl">{element}</h3>
-                            <p className="p-1">Aqui hay poco contenido descriptivo, pero que mala suerte, yo queria más</p>
-                            <button className="bg-sky-500 w-fit self-center py-1 justify-self-center hover:bg-sky-700 text-white font-bold text-xs md:text-sm px-2 md:px-4 rounded"><Link to={"/backoffice"}>MÁS INFORMACION</Link></button>
+                            <p className="p-1">CreatedAT</p>
+                            <div className="grid grid-cols-2 gap-2">
+                                <button className="bg-sky-500 w-fit self-center py-1 justify-self-center hover:bg-sky-700 text-white font-bold text-xs md:text-sm px-2 md:px-4 rounded"><Link to={"/backoffice"}>Editar</Link></button>
+                                <button className="bg-red-500 w-fit self-center py-1 justify-self-center hover:bg-red-700 text-white font-bold text-xs md:text-sm px-2 md:px-4 rounded"><Link to={"/backoffice"}>Eliminar</Link></button>
+                            </div>
+                        
                         </div>
 
                     )
