375ce4ba963441cf2db784e4d26df894dc3c888c
Diego Sosa <dhsosa98@gmail.com>
2022-02-10 08:34:54 -0300

    created get, post, update categories services

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { API_BASE_URL, createMultiForm } from "./index";
+import { API_BASE_URL } from "./index";
+axios.defaults.headers.common["Authorization"] = 'Bearer '+JSON.parse(localStorage.getItem('userData'))?.token;
 
 
 export async function deleteCategorie(id) {
@@ -8,3 +9,20 @@ export async function deleteCategorie(id) {
     .catch((error) => error);
 }
 
+export async function createCategory(category) {
+  return await axios
+    .post(`${API_BASE_URL}/api/v1/categories`, category)
+    .catch((error) => error);
+}
+
+export async function updateCategory(category, id) {
+  return await axios
+    .put(`${API_BASE_URL}/api/v1/categories/${id}`, category)
+    .catch((error) => error);
+}
+
+export async function getCategory(id) {
+  return await axios
+    .get(`${API_BASE_URL}/api/v1/categories/${id}`)
+    .catch((error) => error);
+}
