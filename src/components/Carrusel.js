cc060e0fcd7a4b68a43c6648966ff5bfeff742c3
Rafael Lynch <rafalynch.dev@gmail.com>
2022-02-11 00:09:02 -0300

    Styles

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -20,11 +20,11 @@ function Carrusel() {
     carouselLabelContainer:
       "absolute flex justify-center w-full bottom-[3%] border-black",
     carouselLabel:
-      "z-10 inline rounded-md text-white px-5 py-3 bg-[rgb(0,0,0,0.6)] transition-all hover:bg-[rgb(0,0,0,1)]",
+      "z-10 inline rounded text-white px-5 py-3 bg-[rgb(14,165,233,1)] ",
     prevArrow:
-      "absolute flex justify-center items-center h-12 w-12 z-10 cursor-pointer text-5xl text-white transition-all duration-200 rounded-full top-[50%] left-[5%] bg-[rgb(0,0,0,0.1)] hover:bg-[rgb(0,0,0,1)]",
+      "absolute flex justify-center items-center h-12 w-12 z-10 cursor-pointer text-5xl text-white transition-all duration-200 rounded-full top-[50%] left-[5%] bg-[rgb(14,165,233,0.3)] hover:bg-[rgb(14,165,233,1)]",
     nextArrow:
-      "absolute flex justify-center items-center h-12 w-12 z-10 cursor-pointer text-5xl text-white transition-all duration-200 rounded-full top-[50%] right-[5%] bg-[rgb(0,0,0,0.1)] hover:bg-[rgb(0,0,0,1)]",
+      "absolute flex justify-center items-center h-12 w-12 z-10 cursor-pointer text-5xl text-white transition-all duration-200 rounded-full top-[50%] right-[5%] bg-[rgb(14,165,233,0.3)] hover:bg-[rgb(14,165,233,1)]",
   };
 
   const prevArrow = (handler) => {
