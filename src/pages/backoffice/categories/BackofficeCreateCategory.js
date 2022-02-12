17d5cb763a277ed98b7ca45efd60ad08346b62c1
Diego Sosa <dhsosa98@gmail.com>
2022-02-12 19:18:58 -0300

    added headers

--- src/pages/backoffice/categories/BackofficeCreateCategory.js
+++ src/pages/backoffice/categories/BackofficeCreateCategory.js
@@ -1,13 +1,12 @@
 import React from "react";
 import CUCategoriesForm from "../../../components/CUCategoriesForm";
+import CUCategoriesHeader from "../../../components/CUCategoriesHeader";
 
 export default function BackofficeCreateCategory() {
   return (
     <div className=" container mx-auto flex justify-center shadow-lg sm:py-40">
       <div className="sm:px-32 px-2 w-full">
-        <h1 className=" text-3xl my-5 text-center text-[#9ac9fb]">
-          Crear una nueva categoria
-        </h1>
+        <CUCategoriesHeader title={"Crear una categoria"} />
         <CUCategoriesForm isEdit={false} />
       </div>
     </div>
