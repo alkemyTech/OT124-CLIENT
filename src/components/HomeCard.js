51b9bf3008484039611725e57ac08f55d63bdac3
Rafael Lynch <rafalynch.dev@gmail.com>
2022-02-12 19:32:28 -0300

    Home card styling

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { API_BASE_URL } from "../services";
 import exampleImage from "../img/3.jpg";
 
 export default function HomeCard(props) {
-  const styles = "w-[200px] h-[150px]";
+  const styles = "w-[200px] h-[150px] object-cover";
 
-  return <img className={styles} src={props.img}></img>;
+  return (
+    <img
+      className={styles}
+      src={`${API_BASE_URL}/api/v1/files/${props.img}`}
+      alt={props.name}
+    ></img>
+  );
 }
