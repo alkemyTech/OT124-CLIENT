807e985272f928f89fa2fc069598ca4e4bf01e43
Diego Sosa <dhsosa98@gmail.com>
2022-02-12 12:27:45 -0300

    added and created header

--- src/pages/backoffice/news/BackofficeEditNew.js
+++ src/pages/backoffice/news/BackofficeEditNew.js
@@ -1,13 +1,12 @@
 import React from "react";
 import CUNewsForm from "../../../components/CUNewsForm";
+import NewsHeader from "../../../components/NewsHeader";
 
 export default function BackofficeEditNews() {
   return (
     <div className=" container mx-auto flex justify-center shadow-lg sm:py-40">
       <div className="sm:px-32 px-2 w-full">
-        <h1 className=" text-3xl my-5 text-center text-[#9ac9fb]">
-          Modificar Novedad
-        </h1>
+        <NewsHeader title={"Modificar una novedad"} />
         <CUNewsForm isEdit={true} />
       </div>
     </div>
--- src/pages/backoffice/testimonials/BackofficeEditTestimonials.js
+++ src/pages/backoffice/testimonials/BackofficeEditTestimonials.js
@@ -1,13 +1,12 @@
 import React from "react";
 import CUTestimonialsForm from "../../../components/CUTestimonialsForm";
+import TestimonialsHeader from "../../../components/TestimonialsHeader";
 
 export default function BackofficeEditTestimonials() {
   return (
     <div className=" container mx-auto flex justify-center shadow-lg sm:py-40">
       <div className="sm:px-32 px-2 w-full">
-        <h1 className=" text-3xl my-5 text-center text-[#9ac9fb]">
-           Modificar un testimonio
-        </h1>
+        <TestimonialsHeader title={"Modificar un testimonio"} />
         <CUTestimonialsForm isEdit={true} />
       </div>
     </div>
