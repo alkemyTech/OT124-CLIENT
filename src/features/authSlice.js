378ab3c640525bca2e1789039ea5371cb348bd10
Diego Sosa <dhsosa98@gmail.com>
2022-02-12 06:00:12 -0300

    added only token in localstorage and only user in redux

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,17 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+import { isExpired, decodeToken } from "react-jwt";
+const token = localStorage.getItem("userData")
+axios.defaults.headers.common["Authorization"] = token ? `Bearer ${token}` : null;
 
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
-    userData: JSON.parse(localStorage.getItem("userData") || null),
+    userData: token ? decodeToken(token)?.user : null,
   },
   reducers: {
     setUserData: (state, action) => {
-      state.userData = action.payload;
-      localStorage.setItem("userData", JSON.stringify(state?.userData));
-      const token = state.userData?.token
-      axios.defaults.headers.common["Authorization"] = token ? `Bearer ${token}` : null;
+        const {token} = action.payload
+        const decoded = decodeToken(token)
+        console.log(decoded)
+        const {user} = decoded
+        state.userData = user;  
+        localStorage.setItem("userData", token);
+        axios.defaults.headers.common["Authorization"] = token ? `Bearer ${token}` : null;
     },
     deleteUserData: (state) => {
       localStorage.removeItem("userData");
