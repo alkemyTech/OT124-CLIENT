0d6a15c2b356f6fd6e4b82c703f08ead003b198f
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-02-16 17:57:03 -0300

    Fixing ReqData

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -17,6 +17,7 @@ export default function Profile() {
       const res = await profileDelete();
   
       if (res.status === 200) {
+        localStorage.setItem("userData", "");
         navigate("/");
       } else {
           console.log("Error 500")
@@ -56,29 +57,29 @@ export default function Profile() {
             : 
             <div className="grid grid-rows-3 row-start-1 row-end-4">
               <div className="grid gap-1 grid-rows-2 md:grid-cols-[minmax(175px,175px)_minmax(225px,1fr)] grid-cols-[minmax(75px,75px)_minmax(125px,1fr)] m-2 row-start-1 row-end-2 col-start-1 col-end-3 ">
-                <h2 className=" col-start-2 col-end-3 row-start-1 row-end-2 font-bold md:text-3xl text-xl">{profile === undefined ? null : profile.lastname + " " + profile.firstname}</h2>
-                <h3 className=" col-start-2 col-end-3 row-start-2 row-end-3 md:text-xl text-sm">{profile === undefined ? null : profile.email}</h3>
+                <h2 className=" col-start-2 col-end-3 row-start-1 row-end-2 font-bold md:text-3xl text-xl">{profile === undefined ? "Cargando datos..." : profile.lastname + " " + profile.firstname}</h2>
+                <h3 className=" col-start-2 col-end-3 row-start-2 row-end-3 md:text-xl text-sm">{profile === undefined ? "Cargando datos..." : profile.email}</h3>
               </div>
               <div className="grid gap-2 grid-cols-2 row-start-3 row-end-4 col-start-1 col-end-3 justify-items-center items-center">
             {popUp.visible ? null :
               <>
                 <button onClick={() => setPopUp({visible:true, operation: "Editar datos"})} className=" w-24 h-12 border rounded border-sky-500 font-semibold hover:text-white hover:bg-sky-500 text-sky-500">Editar</button>
                 <DeleteAlert 
-                                    id="1"
-                                    styles="w-24 h-12 border rounded border-red-500 font-semibold hover:text-white hover:bg-red-500 text-red-500"
-                                    isLoad={isLoad}
-                                    title={"ELIMINAR"}
-                                    message={
-                                      "¿Está seguro que desea eliminar su cuenta?"
-                                    }
-                                    afterMessage={
-                                      "Su cuenta ha sido eliminada exitosamente"
-                                    }
-                                    setIsLoad={setIsLoad}
-                                    service={handleDelete}
-                                    >
-                                    ELIMINAR
-                                    </DeleteAlert>
+                  id="1"
+                  styles="w-24 h-12 border rounded border-red-500 font-semibold hover:text-white hover:bg-red-500 text-red-500"
+                  isLoad={isLoad}
+                  title={"ELIMINAR"}
+                  message={
+                    "¿Está seguro que desea eliminar su cuenta?"
+                  }
+                  afterMessage={
+                    "Su cuenta ha sido eliminada exitosamente"
+                  }
+                  setIsLoad={setIsLoad}
+                  service={handleDelete}
+                  >
+                  ELIMINAR
+                </DeleteAlert>
               </>
             }
               </div>
