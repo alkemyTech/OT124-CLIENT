2206c445090ac6dbc8509ddb6a52a9edbc2fccf9
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-02-17 17:03:44 -0300

    Adding Comp NewsList&Header

---
+++ src/components/NewsHeader.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const BackArrow = "<"
+
+function NewsHeader() {
+  return (
+    <div className="flex sm:flex-row flex-col  items-center justify-around my-7">
+      <h1 className="sm:text-5xl text-3xl text-center text-sky-500">
+        Novedades
+      </h1>
+      <div className="py-10 text-center">
+        <Link
+          to={`crear-novedad`}
+          className=" bg-green-600 text-sm sm:text-base text-white shadow shadow-green-800 rounded-sm px-8 py-4 hover:bg-green-700 transform ease-in-out duration-300 hover:scale-105"
+        >
+          Añadir nueva novedad
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NewsHeader;
