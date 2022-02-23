1c1b4ee299080c43858dcd50bf1b3ed680c2b153
Rafael Lynch <rafalynch.dev@gmail.com>
2022-02-23 13:01:59 -0300

    Refactor signup and fix login and signup

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SignUpForm from "../components/Auth/SignUpForm";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { selectUserData } from "../features/authSlice";
 
 export default function SignUp() {
+  const navigate = useNavigate();
+  const userData = useSelector(selectUserData);
+
+  useEffect(() => {
+    if (userData) {
+      navigate("/");
+    }
+  }, [userData]);
+
   return (
-    <div>
-      <p className="text-2xl font-bold text-center">Registrate</p>
-      <div className="flex justify-center p-2">
+    <div className="flex flex-col self-center justify-center justify-items-center p-2 w-96">
+      <p className="text-2xl font-bold text-center">Registrarse</p>
+      <div className="flex justify-center p-2 max-w-md">
         <SignUpForm />
       </div>
     </div>
