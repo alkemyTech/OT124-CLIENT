62e0e72ec6ae035e9ca3e8ff5f36977b343ed8a5
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-02-23 10:27:58 -0300

    removing Auto-Admin from Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,8 +17,6 @@ function Box() {
 function Home(props) {
   const welcome = "Texto de bienvenida";
 
-  localStorage.setItem('userData', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxMSwiZmlyc3ROYW1lIjoiVXNlciAxMSIsImxhc3ROYW1lIjoiRG9lIiwiZW1haWwiOiJleGFtcGxlMTFAZXhhbXBsZS5jb20iLCJyb2xlIjoiYWRtaW4ifSwiaWF0IjoxNjQ0OTg1MjAwfQ.9AP-Q4OQcFoTmlu0icT_kLtRmz8ZAXyiqMoZbvJaL_Y')
-
   return (
     <div className="Home flex flex-col justify-center items-center text-center">
       <Carrusel />
