0ac042c18477f2d724cc02b2da898af6c3b2bb76
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-02-25 18:43:22 -0300

    FixingPublicAreas

--- src/components/Activities/Cards.js
+++ src/components/Activities/Cards.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { API_BASE_URL } from "../../services";
 
-const Cards = ({ activity }) => {
+const Cards = ({ activity }) => { 
   return (
+    
     <div class="p-10">
       <div class="max-w-sm rounded-lg overflow-hidden shadow-lg">
+        {(activity?.image?.key !== null || activity?.image?.key !== undefined) ? 
         <img
           class="w-full object-cover "
-          src={`${activity.image}`}
-          alt=""
+          src={activity?.image?.key && `${API_BASE_URL}/api/v1/files/${activity?.image?.key}`}
+          alt={activity?.name}
         />
+        :
+        <img
+          class="w-full object-cover "
+          src=""
+          alt="No hay imagen"
+        />
+        }
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">{activity.name}</div>
           <p
@@ -17,9 +28,12 @@ const Cards = ({ activity }) => {
           ></p>
         </div>
         <div class="px-6 pt-4 pb-2">
-          <button className="bg-green-500 text-white px-4 py-1 rounded-full">
-            Ver Más
-          </button>
+          <Link to={`${activity?.id}` }>
+            <button className=" hover:bg-green-700 bg-green-500 text-white px-4 py-1 rounded-full">
+              Ver Más
+            </button>
+          </Link>
+        
           {/* <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
             #photography
           </span>
--- src/components/NewDatails.js
+++ src/components/NewDatails.js
@@ -1,17 +1,21 @@
 import {getNew} from "../services/news";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { API_BASE_URL } from "../services";
 
 export default function NewDetails() {
 
-    const { newId } = useParams();
+    const { id } = useParams();
     const [selectedNew, setSelectedNew] = useState();
+
+    console.log(id)
 
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await getNew (newId.id);
-                setSelectedNew(response.data)
+                const response = await getNew (id);
+                console.log(response)
+                setSelectedNew(response?.data?.new)
             } catch (e) {
                 console.error(e);
             }
@@ -20,10 +24,10 @@ export default function NewDetails() {
     }, []);
 
     return (
-        <section className="flex flex-col m-4 p-4 shadow-lg">
+        <section className="flex flex-col mx-8 my-4 p-4 shadow-lg">
             {selectedNew !== undefined ?
                     <div className="grid rounded md:grid-cols-[2fr,1fr,1fr] grid-cols-1 grid-rows-[200px,100px,180px]">
-                        <img className=" md:row-start-1 md:row-end-3 "  src={selectedNew.img}></img>
+                        <img className=" md:row-start-1 md:row-end-3 " src={selectedNew?.image?.key && `${API_BASE_URL}/api/v1/files/${selectedNew?.image?.key}`}></img>
                         <h3 className=" self-center text-xl md:col-start-2 md:col-end-4 md:row-start-1 md:row-end-2">{selectedNew.name}</h3>
                         <p className=" overflow-y-scroll overflow-x-hidden md:col-start-2 md:col-end-4 md:row-start-2 md:row-end-3">{selectedNew.content}</p>
                     </div>
