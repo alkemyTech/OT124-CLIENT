79f18f564bec775db161e4a2515175bacbd8e8f5
Diego Sosa <dhsosa98@gmail.com>
2022-02-26 16:45:19 -0300

    fixed search issues

--- src/hooks/useQueries.js
+++ src/hooks/useQueries.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { useSelector } from "react-redux";
-import { selectPage, selectSize } from "../features/paginationSlice";
-import { selectSearch } from "../features/searchSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { useLocation } from "react-router";
+import { initialPagination, selectPage, selectSize } from "../features/paginationSlice";
+import { initialSearch, selectSearch } from "../features/searchSlice";
 
 function useQueries() {
   const page = useSelector(selectPage);
   const size = useSelector(selectSize);
   const search = useSelector(selectSearch);
+  const dispatch = useDispatch()
+  const location = useLocation()
 
   const [queries, setQueries] = useState(new URLSearchParams({ page, size }));
 
   useEffect(() => {
     setQueries(new URLSearchParams({ page, size, search }));
   }, [page, size, search]);
+
+  useEffect(()=>{
+        dispatch(initialSearch())
+        dispatch(initialPagination())
+  }, [location, dispatch])
 
   return "?" + queries.toString();
 }
