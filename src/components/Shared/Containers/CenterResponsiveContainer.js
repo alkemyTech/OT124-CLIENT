4f4f3e031b7fdcdb421b4c69acc34423b4d8bd50
Diego Sosa <dhsosa98@gmail.com>
2022-02-25 21:58:15 -0300

    fixed issues

--- src/components/Shared/Containers/CenterResponsiveContainer.js
+++ src/components/Shared/Containers/CenterResponsiveContainer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function CenterResponsiveContainer({ children }) {
   return (
-    <div className="mx-auto flex justify-center h-full bg-sky-500 sm:py-10">
-      <div className="sm:w-auto w-full bg-white h-full sm:rounded-md sm:p-10">
+    <div className="sm:px-10 px-3 flex justify-center h-full sm:rounded-none rounded-md bg-sky-500 sm:py-10 py-3">
+      <div className="lg:w-auto w-full bg-white h-full sm:rounded-md py-20 sm:p-10">
           {children}
       </div>
     </div>
--- src/components/Shared/Table/TableLayout.js
+++ src/components/Shared/Table/TableLayout.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function TableLayout({ children }) {
   return (
-    <div className="overflow-x-auto sm:overflow-x-hidden overflow-y-auto max-h-[450px]">
-          <table className="text-left transform ease-in-out hover:-translate-x-1 rounded-md duration-200 sm:min-w-[1000px] sm:overflow-x-hidden overflow-x-auto cursor-pointer">
+    <div className="overflow-x-auto mx-1 sm:overflow-x-hidden overflow-y-auto max-h-[450px]">
+          <table className="text-left transform ease-in-out hover:-translate-x-1 rounded-md duration-200 sm:min-w-[1000px] max-w-[400px] sm:overflow-x-hidden overflow-x-auto cursor-pointer">
             {children}
           </table>
     </div>
