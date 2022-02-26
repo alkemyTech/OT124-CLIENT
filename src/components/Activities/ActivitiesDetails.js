9a65ff9042fbc7a05b562a9a489d6fd66e198b1f
Francisco Ríos Cabello <francisco.rios.cabello@gmail.com>
2022-02-26 15:04:34 -0300

    Added details for an activity (#98)

---
+++ src/components/Activities/ActivitiesDetails.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getActivity } from "../../services/activities";
+import { API_BASE_URL } from "../../services";
+
+const ActivitiesDetails = () => {
+  const { id } = useParams();
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    getActivity(id)
+      .then((res) => {
+        setActivities(res?.data?.activity);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [id]);
+
+  return (
+    <div className="mx-20">
+      <h1 className="text-center font-bold text-4xl mb-6">
+        {" "}
+        {activities.name}
+      </h1>
+      <img
+        src={`${API_BASE_URL}/api/v1/files/${activities?.image}`}
+        alt=""
+        className="w-full float-left mr-6 mb-8 sm:w-80 h-80"
+      />
+      <p
+        className="font-normal text-lg mb-6"
+        dangerouslySetInnerHTML={{ __html: activities.content }}
+      ></p>
+    </div>
+  );
+};
+
+export default ActivitiesDetails;
