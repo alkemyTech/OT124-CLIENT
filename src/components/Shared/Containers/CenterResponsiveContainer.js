7faf49fc0da599aeb0417a4e88ecb0edb1b013db
Diego Sosa <dhsosa98@gmail.com>
2022-02-27 20:54:02 -0300

    fixed responsive issues

--- src/components/Shared/Containers/CenterResponsiveContainer.js
+++ src/components/Shared/Containers/CenterResponsiveContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 function CenterResponsiveContainer({ children }) {
   return (
     <div className="sm:px-10 px-3 grid lg:h-full justify-center items-center sm:rounded-none rounded-md bg-sky-500 py-10">
-      <div className="lg:w-auto w-full flex flex-col justify-center bg-slate-50 sm:rounded-md py-20 sm:p-10">
+      <div className="grid justify-center bg-slate-50 sm:rounded-md py-20 sm:p-10">
           {children}
       </div>
     </div>
