f97acbf339588c348ba64e0da24d519b8ab691cb
Diego Sosa <dhsosa98@gmail.com>
2022-02-27 20:17:22 -0300

    fixed bugs

--- src/components/Backoffice/BackOfficeCard.js
+++ src/components/Backoffice/BackOfficeCard.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export default function BackOfficeCard(props) {
   return (
-    <div className="mx-8  md:w-60 w-32 md:h-60 h-32 border-1 rounded-lg p-2 md:p-6 grid justify-items-center gap-2 shadow-lg hover:shadow-2xl transform ease-in-out duration-300 hover:scale-105">
+    <div className="mx-8 lg:w-52 h-32 lg:h-52 w-32 border-1 rounded-lg p-2 md:p-6 grid justify-items-center gap-2 shadow-lg hover:shadow-2xl transform ease-in-out duration-300 hover:scale-105">
       <span>{props.title}</span>
-      <img src={props.image} className="h-10 md:h-20"></img>
+      <img src={props.image} className="h-10 lg:h-20"></img>
       <Link
         className=""
         to={props.navlink}
--- src/components/Shared/Table/TableLayout.js
+++ src/components/Shared/Table/TableLayout.js
@@ -3,7 +3,7 @@ import React from "react";
 function TableLayout({ children }) {
   return (
     <div className="overflow-x-auto mx-1 sm:overflow-x-hidden overflow-y-auto max-h-[450px] pl-3">
-          <table className="text-left transform ease-in-out hover:-translate-x-1 rounded-md duration-200 sm:min-w-[1000px] max-w-[400px] sm:overflow-x-hidden overflow-x-auto cursor-pointer">
+          <table className="text-left transform ease-in-out hover:-translate-x-1 rounded-md duration-200 sm:overflow-x-hidden overflow-x-auto cursor-pointer">
             {children}
           </table>
     </div>
