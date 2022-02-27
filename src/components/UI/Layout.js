4fd6423161d226331d20d2f8c5b96ba2bc388970
Diego Sosa <dhsosa98@gmail.com>
2022-02-26 23:21:49 -0300

    created, styled and linked backofficeNavbar

--- src/components/UI/Layout.js
+++ src/components/UI/Layout.js
@@ -8,6 +8,7 @@ import { selectIsExpired, setIsExpired, getIsExpired, selectUserData } from "../
 import ExpiredSessionAlert from "../Shared/Alerts/ExpiredSessionAlert";
 import { useEffect } from "react";
 import { useState } from "react";
+import HeaderUser from "./HeaderUser";
 
 
 export default function Layout() {
@@ -45,7 +46,7 @@ export default function Layout() {
   return (
     <div className="flex flex-col justify-between min-h-[100vh]">
       {(isExpired) && <ExpiredSessionAlert />}
-      <Header />
+      {location.pathname.indexOf("/backoffice")===-1 ? <Header /> : <HeaderUser />}
       <div className="h-full">
         <TransitionGroup component={null}>
           <CSSTransition key={location.key} classNames="fade" timeout={300}>
