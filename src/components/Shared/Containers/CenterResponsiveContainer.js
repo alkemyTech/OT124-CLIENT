43b52b57c178f1a74fd30681a1a1ca05dc71a185
Diego Sosa <dhsosa98@gmail.com>
2022-02-27 18:51:40 -0300

    fixed layout issue

--- src/components/Shared/Containers/CenterResponsiveContainer.js
+++ src/components/Shared/Containers/CenterResponsiveContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function CenterResponsiveContainer({ children }) {
   return (
-    <div className="sm:px-10 px-3 flex sm:h-full justify-center sm:rounded-none rounded-md bg-sky-500 sm:py-10 py-3">
+    <div className="sm:px-10 px-3 flex lg:h-full justify-center sm:rounded-none rounded-md bg-sky-500 sm:py-10 py-3">
       <div className="lg:w-auto w-full bg-slate-50 h-full sm:rounded-md py-20 sm:p-10">
           {children}
       </div>
