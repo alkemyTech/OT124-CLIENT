7fc6735869d31f4020d10b967c6228ca5c0b33b5
Rafa Lynch <rafalynch.dev@gmail.com>
2022-02-28 18:33:37 -0300

    Testimonials cards (#107)

---
+++ src/components/Testimonials/TestimonialsCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { API_BASE_URL } from "../../services";
+
+export default function TestimonialsCard({ testimonial }) {
+  return (
+    <div className="w-[250px] py-4 px-8 bg-white shadow-lg hover:shadow-xl rounded-lg mt-20 grid">
+      <div className="flex justify-center -mt-16">
+        <img
+          className="w-20 h-20 object-cover rounded-full border-2 border-sky-400"
+          alt={testimonial.name}
+          src={`${API_BASE_URL}/api/v1/files/${testimonial.image.key}`}
+        />
+      </div>
+      <div>
+        <p className="mt-2 text-gray-600 italic">"{testimonial.content}"</p>
+      </div>
+      <Link
+        className="flex justify-end mt-4 self-end"
+        to={"/testimonios/" + testimonial.id}
+      >
+        <a className="text-xl font-medium text-sky-500">{testimonial.name}</a>
+      </Link>
+    </div>
+  );
+}
