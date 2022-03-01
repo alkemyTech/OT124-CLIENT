b6e266397d44085db3f4bfc693f2585beea5293a
Franco Martin <francomartin.personal@gmail.com>
2022-03-01 20:56:55 -0300

    added table donators and icon mp

--- src/components/Contribuye/Donators.js
+++ src/components/Contribuye/Donators.js
@@ -8,11 +8,46 @@ import TableLayout from '../Shared/Table/TableLayout'
 function Donators() {
     const [donates, setDonates] = useState()
     const [isLoad, setIsLoad] = useState(false)
+
     useEffect(() => {
+
         async function fetchData() {
+
             let donatesRes = await getDonatesAll()
-            console.log(donatesRes)
-            setDonates(donatesRes?.data)
+            let donatesResNew = donatesRes?.data.map(e => {
+                let donates = Object.create(e.donates)
+                donates.images = {key:''}
+                switch (donates.award) {
+                    case 'gold':
+                        donates.images.key = '9ab88778-2003-4e7f-8f15-fca80f7c5350'
+                        break;
+                    case 'silver':
+                        donates.images.key = '83294e9c-5b62-4011-8b0c-d8a6f91b578f'
+                        break;
+                    case 'bronce':
+                        donates.images.key = 'bf06bd1e-2637-4a60-bd57-37260948a3b6'
+                        break;
+                        case 'noobie':
+                            donates.images.key = '79c191b9-b73f-4ea6-bf36-2241fe3904ce'
+                            break;
+                    default:
+                        donates.images.key ='79c191b9-b73f-4ea6-bf36-2241fe3904ce'
+                        break;
+                }
+
+                delete e["donates"]
+                delete e["role"]
+                delete e["email"]
+                return {
+                    ...e,
+                    amounts: donates.amounts,
+                    image:   donates.images
+
+                }
+
+            })
+            console.log(donatesResNew)
+            setDonates(donatesResNew)
 
         }
         fetchData()
@@ -23,32 +58,31 @@ function Donators() {
     }, [])
 
     return (
+        <div className='min-w-full w-full'>
+            <TableLayout notBackOffice={true}>
 
-        <TableLayout>
-            <HeaderList
-                title={"Donadores"}
-                name={"donadores"}
-            />
-            <HeaderTable
-                columnsName={[
-                    "nombre",
-                    "apellido",
-                    "Imagen",
-                    "Email",
-                    "Fecha",
-                ]}
-            />
-            {/* <BodyTable
-                list={donates}
-                bodyName={"organizacion"}
-                message={"¿Desea eliminar esta organización?"}
-                afterMessage={"organización eliminada con éxito"}
-                isLoad={isLoad}
-                setIsLoad={setIsLoad}
-                service={getDonatesAll}
-              /> */}
-        </TableLayout>
+                <HeaderTable
+                    notBackOffice={true}
+                    columnsName={[
+                        "nombre",
+                        "apellido",
+                        "cantidad",
+                        "premio",
 
+                    ]}
+                />
+                <BodyTable
+                    notBackOffice={true}
+                    list={donates}
+                    bodyName={"organizacion"}
+                    message={"¿Desea eliminar esta organización?"}
+                    afterMessage={"organización eliminada con éxito"}
+                    isLoad={isLoad}
+                    setIsLoad={setIsLoad}
+
+                />
+            </TableLayout>
+        </div>
     )
 }
 
--- src/components/Contribuye/WelcomeMessage.js
+++ src/components/Contribuye/WelcomeMessage.js
@@ -3,98 +3,105 @@ import useMercadoPago from "../../hooks/useMercadopago";
 import { getDonate } from "../../services/contribuye";
 import SendButton from "../Shared/Buttons/SendButton";
 import Donators from "./Donators";
-
+import imgMP from '../../img/logo-MP.png'
 function WelcomeMessage({ onClickActive }) {
-  // let { amount, setAmount } = useMercadoPago()
-  let [thinkAmount, setThinkAmount] = useState("$500");
-  let [color, setColor] = useState(false);
-  const [donate, setDonate] = useState();
+    // let { amount, setAmount } = useMercadoPago()
+    let [thinkAmount, setThinkAmount] = useState("$500");
+    let [color, setColor] = useState(false);
+    const [donate, setDonate] = useState();
 
-  useEffect(() => {
-    async function fetchData() {
-      let donateRes = await getDonate();
-      console.log(donate);
-      setDonate(donateRes?.data);
-    }
-    fetchData();
-    return () => {
-      fetchData();
+    useEffect(() => {
+        async function fetchData() {
+            let donateRes = await getDonate();
+            console.log(donate);
+            setDonate(donateRes?.data);
+        }
+        fetchData();
+        return () => {
+            fetchData();
+        };
+    }, []);
+
+    let amountsArray = [500, 1000, 2000];
+    let styles = {
+        title: "text-4xl text-emerald-500 text-center",
+        amounts: "text-4xl text-sky-500 text-center",
+        text: "text-xl text-slate-600 m-8",
+        set: "text-slate-300 font-black	text-3xl",
     };
-  }, []);
-
-  let amountsArray = [500, 1000, 2000];
-  let styles = {
-    title: "text-4xl text-emerald-500 text-center",
-    amounts: "text-4xl text-sky-500 text-center",
-    text: "text-xl text-slate-600 m-8",
-    set: "text-slate-300 font-black	text-3xl",
-  };
-  function setAmountFun(e) {
-    setThinkAmount(e.currentTarget.textContent);
-  }
-  function establecerMontoDefinitivo() {
-    setColor(true);
-  }
-  return (
-    <div className="">
-      <div className="flex flex-row bg-transparent  text-sky-500 rounded py-4 my-8 px-4  border border-sky-500 justify-evenly">
-        {donate ? (
-          <>
-            <h4 className={styles.title}>{donate?.award}</h4>
-            <h4 className={styles.amounts}>Aportes: {donate?.amounts}</h4>
-          </>
-        ) : (
-          <h4 className={styles.amounts}>No tiene donaciones aún</h4>
-        )}
-      </div>
-
-      <h4 className={styles.title}>Contribuye para bien</h4>
-      <p className={styles.text}>
-        La colaboración es un pilar importante para mantener en pie
-        organizaciones. los aportes estan destinados para insumos y intrumentos,
-        utensillos y herramientas.
-      </p>
-      <p className={styles.text}>
-        Para esto, es necesario contribuir con lo que uno pueda aportar,
-        pequeños aportes solidarios nos ayudan a subsistir día a día.
-      </p>
-      <div className="flex sm:flex-row flex-col items-center justify-row justify-around">
-        {amountsArray.map((e) => {
-          return (
-            <div className="w-[120px]">
-              <SendButton text={"$" + e} click={setAmountFun} />
+    function setAmountFun(e) {
+        setThinkAmount(e.currentTarget.textContent);
+    }
+    function establecerMontoDefinitivo() {
+        setColor(true);
+    }
+    return (
+        <div className="">
+            <div className="flex flex-row bg-transparent  text-sky-500 rounded py-4 my-8  border border-sky-500 justify-evenly">
+                {donate ? (
+                    <>
+                        <h4 className={styles.title}>{donate?.award}</h4>
+                        <h4 className={styles.amounts}>Aportes: {donate?.amounts}</h4>
+                    </>
+                ) : (
+                    <h4 className={styles.amounts}>No tiene donaciones aún</h4>
+                )}
             </div>
-          );
-        })}
-      </div>
 
-      <div className="flex justify-row justify-around">
-        <p
-          className={
-            "bg-transparent  text-sky-500 font-semibold   rounded py-2 mt-2 px-4 " +
-            (color ? styles.set : null)
-          }
-        >
-          {thinkAmount}
-        </p>
-      </div>
-      <div className="grid justify-center">
-        <div className=" w-[200px] ">
-          {!color ? (
-            <>
-              <SendButton
-                text={"Establecer monto"}
-                click={establecerMontoDefinitivo}
-              />
-            </>
-          ) : (
-            <SendButton text={"Contribuye"} click={onClickActive} />
-          )}
-          {/* <Donators/> */}
+            <h4 className={styles.title}>Contribuye para bien</h4>
+            <p className={styles.text}>
+                La colaboración es un pilar importante para mantener en pie
+                organizaciones. los aportes estan destinados para insumos y intrumentos,
+                utensillos y herramientas.
+            </p>
+            <p className={styles.text}>
+                Para esto, es necesario contribuir con lo que uno pueda aportar,
+                pequeños aportes solidarios nos ayudan a subsistir día a día.
+            </p>
+            <div className="flex sm:flex-row flex-col items-center justify-row justify-around">
+                {amountsArray.map((e) => {
+                    return (
+                        <div className="w-[120px]">
+                            <SendButton text={"$" + e} img={{
+                                url: imgMP,
+                                width: '25',
+                                height: '25'
+                            }} click={setAmountFun} />
+                        </div>
+                    );
+                })}
+            </div>
+
+            <div className="flex justify-row justify-around">
+                <p
+                    className={
+                        "bg-transparent  text-sky-500 font-semibold   rounded py-2 mt-2 px-4 " +
+                        (color ? styles.set : null)
+                    }
+                >
+                    {thinkAmount}
+                </p>
+            </div>
+            <div className="grid justify-center items-center">
+                <div className=" w-[200px] ">
+                    {!color ? (
+                        <>
+                            <SendButton
+                                text={"Establecer monto"}
+                                click={establecerMontoDefinitivo}
+                            />
+                        </>
+                    ) : (
+                        <SendButton text={"Contribuye"} click={onClickActive} />
+                    )}
+                </div>
+
+            </div>
+            <div className="grid justify-center w-full">
+                <Donators />
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default WelcomeMessage;
