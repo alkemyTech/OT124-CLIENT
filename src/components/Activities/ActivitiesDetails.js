c6507d0d5e691fe44d9d46c657205660944587bb
juanpa0011 <juanpablo1996epifani@gmail.com>
2022-03-01 09:07:37 -0300

    Init

--- src/components/Activities/ActivitiesDetails.js
+++ src/components/Activities/ActivitiesDetails.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getActivity } from "../../services/activities";
 import { API_BASE_URL } from "../../services";
+import CenterResponsiveContainer from "../Shared/Containers/CenterResponsiveContainer";
 
 const ActivitiesDetails = () => {
   const { id } = useParams();
@@ -18,21 +19,24 @@ const ActivitiesDetails = () => {
   }, [id]);
 
   return (
-    <div className="mx-20">
-      <h1 className="text-center font-bold text-4xl mb-6">
+    <CenterResponsiveContainer>
+    <div className="md:mx-10 mx-5 grid grid-cols-1 md:grid-cols-2 rounded" >
+      <h3 className="text-center font-bold text-4xl mb-6 md:col-start-1 md:col-end-2">
         {" "}
         {activities.name}
-      </h1>
+      </h3>
       <img
         src={`${API_BASE_URL}/api/v1/files/${activities?.image?.key}`}
         alt=""
-        className="w-full float-left mr-6 mb-8 sm:w-80 h-80"
+        className=" md:col-start-1 md:col-end-2 md:max-w-[600px] md:min-w-[500px] rounded justify-self-center self-center"
       />
       <p
-        className="font-normal text-lg mb-6"
+        className=" md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3 font-normal text-lg mt-6 overflow-y-scroll overflow-x-hidden px-6 md:max-h-full min-h-[100px]"
         dangerouslySetInnerHTML={{ __html: activities.content }}
       ></p>
     </div>
+
+    </CenterResponsiveContainer>
   );
 };
 
--- src/components/NewDatails.js
+++ src/components/NewDatails.js
@@ -2,6 +2,7 @@ import {getNew} from "../services/news";
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { API_BASE_URL } from "../services";
+import CenterResponsiveContainer from "./Shared/Containers/CenterResponsiveContainer";
 
 export default function NewDetails() {
 
@@ -24,18 +25,18 @@ export default function NewDetails() {
     }, []);
 
     return (
-        <section className="flex flex-col mx-8 my-4 p-4 shadow-lg">
+        <CenterResponsiveContainer>
             {selectedNew !== undefined ?
-                    <div className="grid rounded md:grid-cols-[2fr,1fr,1fr] grid-cols-1 grid-rows-[200px,100px,180px]">
-                        <img className=" md:row-start-1 md:row-end-3 " src={selectedNew?.image?.key && `${API_BASE_URL}/api/v1/files/${selectedNew?.image?.key}`}></img>
-                        <h3 className=" self-center text-xl md:col-start-2 md:col-end-4 md:row-start-1 md:row-end-2">{selectedNew.name}</h3>
-                        <p className=" overflow-y-scroll overflow-x-hidden md:col-start-2 md:col-end-4 md:row-start-2 md:row-end-3">{selectedNew.content}</p>
+                    <div className="md:mx-10 mx-5 grid grid-cols-1 md:grid-cols-2 rounded">
+                        <h3 className=" text-center font-bold text-4xl mb-6 md:col-start-1 md:col-end-2">{selectedNew.name}</h3>
+                        <img className=" md:col-start-1 md:col-end-2 md:max-w-[600px] md:min-w-[350px] rounded justify-self-center self-center " src={selectedNew?.image?.key && `${API_BASE_URL}/api/v1/files/${selectedNew?.image?.key}`}></img>
+                        <p className="md:col-start-2 md:col-end-3 md:row-start-1 md:row-end-3 font-normal text-lg mt-6 overflow-y-scroll overflow-x-hidden px-6 md:max-h-full min-h-[100px]">{selectedNew.content}</p>
                     </div>
              : 
                 <div className="grid rounded grid-cols-[1fr] grid-rows-[1fr] justify-items-center">
                     <h3 className=" text-xl ">La novedad no existe o no se encuentra.</h3>
                 </div>
              }
-        </section>
+        </CenterResponsiveContainer>
     )
 }
