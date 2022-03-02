a90af1457db822f0b816dd2ad735f4156fc9e58d
Diego Sosa <dhsosa98@gmail.com>
2022-03-01 21:57:09 -0300

    fixed contribuye styles

--- src/components/Shared/Table/HeaderTable.js
+++ src/components/Shared/Table/HeaderTable.js
@@ -4,8 +4,8 @@ function HeaderTable(props) {
   const { columnsName, notBackOffice } = props;
 
   return (
-    <thead className="bg-sky-500  text-sm sm:text-base z-0">
-      <tr className="uppercase  bg-sky-500 ">
+    <thead className={` text-sm ${notBackOffice ? " bg-emerald-500" : "bg-sky-500"}  sm:text-base z-0`}>
+      <tr className={`uppercase ${notBackOffice ? " bg-emerald-500 text-white" : "bg-sky-500"} `}>
         {columnsName?.map((item) => (
           <th className=" py-3 px-4 w-[170px]">{item}</th>
         ))}
